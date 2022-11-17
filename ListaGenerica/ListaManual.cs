f081fe78d782917de1a7d65f472c80b7d1d7e06f
FelipeCherubin <felipe_cherubin@hotmail.com>
2022-11-17 14:47:31 -0300

    implementação de lista generica

---
+++ ListaGenerica/ListaManual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ListaGenerica
+{
+    public class ListaManual<T>
+    {
+        private T[] ItemLista;
+
+        public ListaManual()
+        {
+            ItemLista = new T[0];
+        }
+
+        public void Add(T novoItem)
+        {
+            T[] novosItens = new T[ItemLista.Length + 1];
+            for (int index = 0; index < ItemLista.Length; index++)
+                novosItens[index] = ItemLista[index];
+            novosItens[novosItens.Length - 1] = novoItem;
+            ItemLista = novosItens;
+        }
+
+        public int Count()
+            => ItemLista.Length;
+
+        public void PrintItens()
+        {
+            foreach (T t in ItemLista)
+                Console.WriteLine(t);
+
+            Console.WriteLine($"Total de itens na lista: {Count()}");
+        }
+    }
+}
---
+++ ListaGenerica/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ListaGenerica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Simples exemplo de criação de uma lista generica de uma forma manual sem usar o System.Collections.Generic
+             */
+            ListaManual<string> listaManual = new();
+            listaManual.Add(novoItem: "LP");
+            listaManual.Add(novoItem: "David");
+            listaManual.Add(novoItem: "Alê");
+            listaManual.Add(novoItem: "Glauber");
+
+            listaManual.PrintItens();
+
+
+            ListaManual<int> listaManual1 = new();
+            listaManual1.Add(novoItem: 1);
+            listaManual1.Add(novoItem: 2);
+            listaManual1.Add(novoItem: 3);
+
+            listaManual1.PrintItens();
+
+            Console.ReadLine();
+        }
+    }
+}
